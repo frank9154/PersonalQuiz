baedba3a6d5671c8cfb1933917eda60fa00f41cb
alexsokolov <frank9154@gmail.com>
2023-12-02 22:05:50 +0300

    Second commit

--- PersonalQuiz/ViewControllers/QuestionsViewController.swift
+++ PersonalQuiz/ViewControllers/QuestionsViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 final class QuestionsViewController: UIViewController {
 
+    //private let questionOne = Question(title: "Ответ один", type: .single, anwswers: [Answer(title: "Ответ один", animal: .dog)])
+    
+    private let questions = Question.getQuestions()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
